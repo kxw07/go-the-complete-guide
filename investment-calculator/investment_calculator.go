41cab239f39f08fc376e2e3dc2ad8baa24d1ecf5
kai <kai@simbo.com.tw>
2024-10-03 20:02:10 +0800

    feat: accept user enter arguments

--- investment-calculator/investment_calculator.go
+++ investment-calculator/investment_calculator.go
@@ -5,11 +5,24 @@ import (
 	"math"
 )
 
+const inflationRate float64 = 2.5
+
 func main() {
-	var investmentAmount float64 = 1000
-	var expectedReturnRate = 5.5
-	var years float64 = 10
+	var investmentAmount float64
+	var expectedReturnRate float64
+	var years float64
+
+	fmt.Print("Enter the investment amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("expected return rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("years: ")
+	fmt.Scan(&years)
 
 	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	var futureRealValue = futureValue / math.Pow(1+inflationRate/100, years)
 	fmt.Printf("Future Value: %.2f\n", futureValue)
+	fmt.Printf("Future Real Value: %.2f\n", futureRealValue)
 }
