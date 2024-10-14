eee83da427350358e94142b55955ff5b847b8d25
kai <kai@simbo.com.tw>
2024-10-14 15:17:19 +0800

    refactor: extract method

--- profit-calculator/profit_calculator.go
+++ profit-calculator/profit_calculator.go
@@ -3,6 +3,17 @@ package main
 import "fmt"
 
 func main() {
+
+	revenue, expenses, tax := getUserInput()
+
+	ebt, profit, ratio := calculate(revenue, expenses, tax)
+
+	fmt.Printf("The EBT is: %.2f\n", ebt)
+	fmt.Printf("The profit is: %.2f\n", profit)
+	fmt.Printf("The ratio is: %.2f\n", ratio)
+}
+
+func getUserInput() (float64, float64, float64) {
 	var revenue float64
 	var expenses float64
 	var tax float64
@@ -16,10 +27,13 @@ func main() {
 	fmt.Print("Enter the tax(%): ")
 	fmt.Scan(&tax)
 
+	return revenue, expenses, tax
+}
+
+func calculate(revenue float64, expenses float64, tax float64) (float64, float64, float64) {
 	var ebt = revenue - expenses
 	var profit = ebt * (1 - tax/100)
 	var ratio = ebt / profit
-	fmt.Printf("The EBT is: %.2f\n", ebt)
-	fmt.Printf("The profit is: %.2f\n", profit)
-	fmt.Printf("The ratio is: %.2f\n", ratio)
+
+	return ebt, profit, ratio
 }
