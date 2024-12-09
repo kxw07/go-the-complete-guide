9b772fa2a7ab657bbd7d71c8f1cdd9f6d16d6d8f
kai <kai@simbo.com.tw>
2024-12-09 21:30:39 +0800

    feat: build constructor

--- structs/structs.go
+++ structs/structs.go
@@ -12,6 +12,26 @@ type User struct {
 	createdAt time.Time
 }
 
+func (u User) outputUserInfo() {
+	fmt.Printf("Your full name is %s %s.\n", u.firstName, u.lastName)
+	fmt.Printf("Your birthday is %s.\n", u.birthday)
+	fmt.Printf("Data was created at %s.\n", u.createdAt)
+}
+
+func (u *User) clearName() {
+	u.firstName = ""
+	u.lastName = ""
+}
+
+func userConstructor(firstName string, lastName string, birthday string) *User {
+	return &User{
+		firstName: firstName,
+		lastName:  lastName,
+		birthday:  birthday,
+		createdAt: time.Now(),
+	}
+}
+
 func main() {
 	// user := User{}
 	// user.firstName = getUserInput("Please enter your first name:")
@@ -22,23 +42,18 @@ func main() {
 	inputLastName := getUserInput("Please enter your last name:")
 	inputBirthday := getUserInput("Please enter your birthday:")
 
-	user := User{
-		firstName: inputFirstName,
-		lastName:  inputLastName,
-		birthday:  inputBirthday,
-		createdAt: time.Now(),
-	}
+	user := userConstructor(inputFirstName, inputLastName, inputBirthday)
 
-	printUser(&user)
-}
+	// user := User{
+	// firstName: inputFirstName,
+	// lastName:  inputLastName,
+	// birthday:  inputBirthday,
+	// createdAt: time.Now(),
+	// }
 
-func printUser(user *User) {
-	fmt.Printf("Your full name is %s %s.\n", (*user).firstName, (*user).lastName)
-	fmt.Printf("Your birthday is %s.\n", (*user).birthday)
-	fmt.Printf("Data was created at %s.\n", (*user).createdAt)
-	// fmt.Printf("Your full name is %s %s.\n", user.firstName, user.lastName)
-	// fmt.Printf("Your birthday is %s.\n", user.birthday)
-	// fmt.Printf("Data was created at %s.\n", user.createdAt)
+	user.outputUserInfo()
+	user.clearName()
+	user.outputUserInfo()
 }
 
 func getUserInput(message string) string {
