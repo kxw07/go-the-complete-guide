9c26611b6fcdd8bddf7906b0e162750573ea5b16
Kai Wu <kaiwu@KaideMac-mini.local>
2025-01-10 14:27:17 +0800

    feat: practice array

---
+++ arrays/arrays.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	prices := [3]float64{}
+	prices[0] = 100.0
+	prices[1] = 200.0
+	prices[2] = 300.0
+	fmt.Println(prices)
+
+	numbers := [3]int{10, 20, 30}
+	fmt.Println(numbers)
+}
---
+++ arrays/go.mod
@@ -0,0 +1,3 @@
+module github.com/kxw07/arrays
+
+go 1.23.4
