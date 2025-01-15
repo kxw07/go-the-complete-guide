09937a65f0e0aebba64198fcd4501b9f4c89154d
Kai Wu <kaiwu@KaideMac-mini.local>
2025-01-15 09:12:22 +0800

    feat: practice slice

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -3,12 +3,23 @@ package main
 import "fmt"
 
 func main() {
+	// arrays
 	prices := [3]float64{}
 	prices[0] = 100.0
 	prices[1] = 200.0
 	prices[2] = 300.0
 	fmt.Println(prices)
 
-	numbers := [3]int{10, 20, 30}
+	// slices
+	var numbers []int
+	numbers = []int{1, 2, 3}
+	fmt.Println(numbers)
+	fmt.Println(numbers[:2])
+	fmt.Println(numbers[1:])
+
+	// slices is reference type
+	slices := numbers[1:2]
+	slices[0] = 25
+	fmt.Println(slices)
 	fmt.Println(numbers)
 }
