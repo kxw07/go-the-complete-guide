f6ba72bee269973ee94b47b7160c8419dfb037d4
Kai Wu <kaiwu@KaideMac-mini.local>
2025-06-23 21:43:58 +0800

    feat: init gin

--- REST-API/main.go
+++ REST-API/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+)
 
+func main() {
+	server := gin.Default()
+	server.GET("/hello", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello, World!",
+		})
+	})
+
+	err := server.Run(":8080")
+	if err != nil {
+		return
+	}
 }
