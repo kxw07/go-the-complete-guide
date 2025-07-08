ab492e8174aac11293b2ce64390ca1d01152d2e8
kxw07@github.com <kaiwu@KaideMac-mini.local>
2025-07-08 21:32:03 +0800

    feat: use DB sqllite to create and get events

--- REST-API/model/event.go
+++ REST-API/model/event.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"github.com/kxw07/REST-API/storage"
+	"log/slog"
 	"time"
 )
 
@@ -13,12 +15,46 @@ type Event struct {
 	DateTime    time.Time `binding:"required"`
 }
 
-var events = []Event{}
-
 func GetAll() []Event {
+	getEvents := `
+	SELECT id, user_id, name, description, location, date_time
+	FROM events
+	`
+
+	rows, err := storage.DB.Query(getEvents)
+	if err != nil {
+		slog.Error("error when querying events", "error", err)
+		return nil
+	}
+
+	defer rows.Close()
+	var events []Event
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.ID, &event.UserID, &event.Name, &event.Description, &event.Location, &event.DateTime)
+		if err != nil {
+			slog.Error("error when scanning row", "error", err)
+			continue
+		}
+		events = append(events, event)
+	}
+
 	return events
 }
 
 func AddEvent(event Event) {
-	events = append(events, event)
+	insertEvents := `
+	INSERT INTO events (id, user_id, name, description, location, date_time)
+	VALUES (?, ?, ?, ?, ?, ?)
+	`
+
+	stmt, err := storage.DB.Prepare(insertEvents)
+	if err != nil {
+		slog.Error("error when preparing statement", "error", err)
+	}
+
+	_, err = stmt.Exec(event.ID, event.UserID, event.Name, event.Description, event.Location, event.DateTime)
+	if err != nil {
+		slog.Error("error when executing statement", "error", err)
+	}
 }
